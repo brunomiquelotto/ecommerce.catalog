83852c2d3735555da5006ebf1de5b1db3ca3930d
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2022-08-14 09:38:08 -0300

    REFACT: Add ServiceCollection Extensions - change Catalog.Worker/program.cs to use extensions - add Worker.Library/Extensions with extension methods

--- Ecommerce.Catalog.Worker/Program.cs
+++ Ecommerce.Catalog.Worker/Program.cs
@@ -1,8 +1,7 @@
-﻿using Ecommerce.Worker.Handlers;
+﻿using Ecommerce.Worker.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System.Reflection;
 
 using var host = Host.CreateDefaultBuilder()
@@ -14,22 +13,9 @@ using var host = Host.CreateDefaultBuilder()
     })
     .ConfigureServices((context, serviceCollection) => {
         serviceCollection.RegisterEasyNetQ(context.Configuration.GetConnectionString("RabbitMq"));
-
-        serviceCollection.Scan(scan => {
-            scan.FromAssemblies(Assembly.GetExecutingAssembly())
-            .AddClasses(classes => classes.AssignableTo<IMessageHandler>())
-            .AsImplementedInterfaces()
-            .WithScopedLifetime()
-            .AddClasses(classes => classes.AssignableTo<IHostedService>())
-            .AsImplementedInterfaces()
-            .WithScopedLifetime();
-        });
-
-        serviceCollection.AddScoped<IMessageHandlerResolver, MessageHandlerResolver>(sp =>
-            new MessageHandlerResolver(sp.GetServices<IMessageHandler>().ToList(), sp.GetRequiredService<ILogger<MessageHandlerResolver>>()));
+        serviceCollection.AddMessageHandlers();
+        serviceCollection.AddHostedServices();
     })
     .Build();
 
 await host.RunAsync();
-
-Console.WriteLine("Worker service started.");
---
+++ Ecommerce.Worker.Library/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.Worker.Handlers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace Ecommerce.Worker.Extensions {
+    public static class ServiceCollectionExtensions {
+        public static IServiceCollection AddMessageHandlers(this IServiceCollection serviceCollection) {
+            serviceCollection.Scan(scan => {
+                scan.FromAssemblies(Assembly.GetEntryAssembly()!)
+                .AddClasses(classes => classes.AssignableTo<IMessageHandler>())
+                .AsImplementedInterfaces()
+                .WithScopedLifetime();
+            });
+
+            serviceCollection.AddScoped<IMessageHandlerResolver, MessageHandlerResolver>(sp =>
+                new MessageHandlerResolver(sp.GetServices<IMessageHandler>().ToList(), sp.GetRequiredService<ILogger<MessageHandlerResolver>>()));
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddHostedServices(this IServiceCollection serviceCollection) {
+            serviceCollection.Scan(scan => {
+                scan.FromAssemblies(Assembly.GetEntryAssembly()!)
+                .AddClasses(classes => classes.AssignableTo<IHostedService>())
+                .AsImplementedInterfaces()
+                .WithScopedLifetime();
+            });
+
+            return serviceCollection;
+        }
+    }
+}
