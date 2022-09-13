9a2017a9a51c6e77f0b2695ca181365e34769c79
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2022-09-12 21:26:08 -0300

    FEATURE: Snowflake twitter id generation - add Infra project

---
+++ Ecommerce.Infraestructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+
+namespace Ecommerce.Infraestructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIdGeneratorClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddRefitClient<IIdGeneratorClient>().ConfigureHttpClient(options =>
+            {
+                options.BaseAddress = new Uri(configuration["IIdGeneratorClientUrl"].ToString());
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Ecommerce.Infraestructure/IIdGeneratorClient.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+
+namespace Ecommerce.Infraestructure
+{
+    public interface IIdGeneratorClient
+    {
+        [Get("id/bulk-create")]
+        Task<IEnumerable<long>> Create(int count);
+        [Get("id/new-id")]
+        Task<long> CreateAsync();
+    }
+}
